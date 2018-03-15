bcff450d8b4e59f06e45d97e7e1dc803a4884e32
RahulSathiri <harsha@harshapc>
2018-03-15 16:28:40 +0530

    after commit the changes in static content

---
+++ src/main/java/nexii5/Helloworld.java
@@ -0,0 +1,15 @@
+package nexii5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloworld {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helloworld.class, args);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/nexii5/HomeController.java
@@ -0,0 +1,24 @@
+package nexii5;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value ="/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute User user) {
+		ModelAndView modelandview = new ModelAndView();
+		modelandview.setViewName("user-data");
+		modelandview.addObject("user", user);
+		return modelandview;
+	}
+
+}
